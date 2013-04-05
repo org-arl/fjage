70f86c0d84d02c5239051d603b7c998d3f1c414e
Mandar Chitre <mandar@arl>
2013-04-06 00:33:33 +0800

    Moved VERSION to a standalone file
    Need to change version number in VERSION file
    and also in src/sphinx/fjage_quickstart.sh

--- src/sphinx/fjage_quickstart.sh
+++ src/sphinx/fjage_quickstart.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
+# fjage version
+VERSION=1.0.1
+
 # create the folder structure
 mkdir -p build/libs etc logs
 
 # download necessary JARs
 cd build/libs
-curl -O http://search.maven.org/remotecontent?filepath=com/github/org-arl/fjage/1.0.1/fjage-1.0.1.jar
+curl -O http://search.maven.org/remotecontent?filepath=com/github/org-arl/fjage/$VERSION/fjage-$VERSION.jar
 curl -O http://search.maven.org/remotecontent?filepath=org/codehaus/groovy/groovy-all/2.1.2/groovy-all-2.1.2.jar
 curl -O http://search.maven.org/remotecontent?filepath=org/apache/commons/commons-lang3/3.1/commons-lang3-3.1.jar
 curl -O http://search.maven.org/remotecontent?filepath=jline/jline/2.10/jline-2.10.jar
-curl -O http://search.maven.org/remotecontent?filepath=uk/com/robust-it/cloning/1.9.0/cloning-1.9.0.jar
-curl -O http://search.maven.org/remotecontent?filepath=org/objenesis/objenesis/1.2/objenesis-1.2.jar
+#curl -O http://search.maven.org/remotecontent?filepath=uk/com/robust-it/cloning/1.9.0/cloning-1.9.0.jar
+#curl -O http://search.maven.org/remotecontent?filepath=org/objenesis/objenesis/1.2/objenesis-1.2.jar
 cd ../..
 
 # download init scripts and logging configuration
