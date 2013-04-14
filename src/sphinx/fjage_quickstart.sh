88ee331eb910ca85d7a9cb5e22961c45908ba312
Mandar Chitre <mandar@arl>
2013-04-15 03:07:18 +0800

    Changed Groovy version to 2.1.3

--- src/sphinx/fjage_quickstart.sh
+++ src/sphinx/fjage_quickstart.sh
@@ -9,7 +9,7 @@ mkdir -p build/libs etc logs
 # download necessary JARs
 cd build/libs
 curl -O http://search.maven.org/remotecontent?filepath=com/github/org-arl/fjage/$VERSION/fjage-$VERSION.jar
-curl -O http://search.maven.org/remotecontent?filepath=org/codehaus/groovy/groovy-all/2.1.2/groovy-all-2.1.2.jar
+curl -O http://search.maven.org/remotecontent?filepath=org/codehaus/groovy/groovy-all/2.1.3/groovy-all-2.1.3.jar
 curl -O http://search.maven.org/remotecontent?filepath=org/apache/commons/commons-lang3/3.1/commons-lang3-3.1.jar
 curl -O http://search.maven.org/remotecontent?filepath=jline/jline/2.10/jline-2.10.jar
 #curl -O http://search.maven.org/remotecontent?filepath=uk/com/robust-it/cloning/1.9.0/cloning-1.9.0.jar
