a704b1a7b02997225e05347d24f20840f7b640fa
Mandar Chitre <mandar@nus.edu.sg>
2013-07-21 22:32:37 +0800

    Added option to start GUI or console shell in fjage.sh

--- etc/initrc.groovy
+++ etc/initrc.groovy
@@ -1,8 +1,11 @@
 import org.arl.fjage.*
 import org.arl.fjage.shell.*
 
+boolean gui = System.properties.getProperty('fjage.gui') == 'true'
+
 platform = new RealTimePlatform()
 container = new Container(platform)
-shell = new ShellAgent(new ConsoleShell(), new GroovyScriptEngine())
+if (gui) shell = new ShellAgent(new SwingShell(), new GroovyScriptEngine())
+else shell = new ShellAgent(new ConsoleShell(), new GroovyScriptEngine())
 container.add 'shell', shell
 platform.start()
--- fjage.sh
+++ fjage.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+#
+# Usage:
+#   ./fjage.sh [-gui] [-nocolor]
 
 CLASSPATH=.`find build/libs -name *.jar -exec /bin/echo -n :'{}' \;`
 
@@ -7,5 +10,22 @@ if [ $(expr "$(uname -s)" : 'CYGWIN.*') -gt 0 ];then
   CLASSPATH=`echo "$CLASSPATH" | sed 's/:/;/g'`
 fi
 
+# process command line options
+GUI=false
+OPT1=
+while [[ $1 == -* ]]
+do
+  OPT=$1
+  if [ $OPT = "-gui" ]
+  then
+    shift
+    GUI=true
+  else
+    shift
+    OPT1="$OPT1 $OPT"
+  fi
+  shift
+done
+
 mkdir -p logs
-java -cp "$CLASSPATH" org.arl.fjage.shell.GroovyBoot $@ etc/initrc.groovy
+java -cp "$CLASSPATH" -Dfjage.gui=$GUI org.arl.fjage.shell.GroovyBoot $@ etc/initrc.groovy
