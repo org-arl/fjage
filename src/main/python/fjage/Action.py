cc9e2d428b3ab0cad37c56447f29169b7e959a82
Prasad Anjangi <prasadtiru@gmail.com>
2017-11-15 10:56:18 +0800

    Removed some files to match the directory structure to the python branch.

--- src/main/python/fjage/Action.py
+++
@@ -1,8 +0,0 @@
-class Action:
-    AGENTS = "agents"
-    CONTAINS_AGENT = "containsAgent"
-    SERVICES = "services"
-    AGENT_FOR_SERVICE = "agentForService"
-    AGENTS_FOR_SERVICE = "agentsForService"
-    SEND = "send"
-    SHUTDOWN = "shutdown"
