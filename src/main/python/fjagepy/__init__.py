e8633d1252829682cfd86d9a3559adac7dd763b6
Prasad Anjangi <prasadtiru@gmail.com>
2017-11-30 22:29:21 +0800

    added the list of packages to be imported with *

--- src/main/python/fjagepy/__init__.py
+++ src/main/python/fjagepy/__init__.py
@@ -5,3 +5,6 @@ from fjagepy.org_arl_fjage import GenericMessage
 from fjagepy.org_arl_fjage_shell import ShellExecReq
 from fjagepy.org_arl_fjage_remote import Gateway
 from fjagepy.org_arl_fjage_remote import Action
+
+
+__all__ = ['org_arl_fjage', 'org_arl_fjage_remote', 'org_arl_fjage_shell']
