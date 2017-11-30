079ae167ba98a7da2c0cd9f37d59c7b8b1591b3e
Prasad Anjangi <prasadtiru@gmail.com>
2017-11-30 09:49:45 +0800

    Added setup.py for configuring the package. Changed the directory structure again.

---
+++ src/main/python/fjage/__init__.py
@@ -0,0 +1,7 @@
+# from fjage.org_arl_fjage import AgentID
+# from fjage.org_arl_fjage import Performative
+# from fjage.org_arl_fjage import Message
+# from fjage.org_arl_fjage import GenericMessage
+# from fjage.org_arl_fjage_shell import ShellExecReq
+# from fjage.org_arl_fjage_remote import Gateway
+# from fjage.org_arl_fjage_remote import Action
