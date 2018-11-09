2272608ae4ea6851d1b788eef03ce3efaff0cb38
Chinmay Pendharkar <notthetup@gmail.com>
2018-11-09 14:30:23 +0800

    added mechanism to complete the test from browser side

--- src/jsTest/groovy/fjagejsTest.groovy
+++ src/jsTest/groovy/fjagejsTest.groovy
@@ -8,19 +8,21 @@ for full license details.
 
 ******************************************************************************/
 
-// package org.arl.fjage.test
 
 import org.arl.fjage.*
-import org.arl.fjage.remote.*
-import org.arl.fjage.shell.*
-import org.arl.fjage.connectors.*
-
+import org.arl.fjage.connectors.Connector
+import org.arl.fjage.connectors.WebServer
+import org.arl.fjage.connectors.WebSocketConnector
+import org.arl.fjage.remote.MasterContainer
 import org.junit.Test
+import static org.junit.Assert.assertTrue
 
 class fjagejsTest {
 
   @Test
   void fjageJSTest() {
+    def testStatus = false
+    def testPending = true
     def platform = new RealTimePlatform()
     def container = new MasterContainer(platform, 5081)
     WebServer.getInstance(8080).add("/", "/org/arl/fjage/web")
@@ -28,7 +30,22 @@ class fjagejsTest {
     Connector conn = new WebSocketConnector(8080, "/shell/ws")
     container.addConnector(new WebSocketConnector(8080, "/ws", true))
     platform.start()
-
-    sleep(50000)
+    container.add('test', new Agent(){
+      @Override
+      protected void init() {
+        add(new MessageBehavior(){
+          @Override
+          void onReceive(Message msg) {
+            println("Received: " + msg.performative + ',' + msg.recipient + ',' + msg.sender)
+            testPending = false
+            testStatus = msg.performative != Performative.FAILURE
+          }
+        })
+      }
+    })
+    while (testPending) platform.delay(1000)
+    platform.shutdown()
+    assertTrue(testStatus)
   }
 }
+
