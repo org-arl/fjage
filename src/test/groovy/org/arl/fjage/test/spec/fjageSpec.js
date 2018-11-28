b31484893d48634118f952440d8a73c559e28472
Chinmay Pendharkar <chinmay@subnero.com>
2018-11-28 16:03:04 +0800

    added more tests

--- src/test/groovy/org/arl/fjage/test/spec/fjageSpec.js
+++ src/test/groovy/org/arl/fjage/test/spec/fjageSpec.js
@@ -11,29 +11,86 @@ describe("A Gateway", function() {
     }
 
     expect(createGW).not.toThrow();
-    expect(gw1).not.toBe(null);
+    expect(gw1).toBeDefined();
   });
 
   it("should have a successfully opened WebSocket connection", function() {
-    var gw1
-    var createGW = function(){
-      gw1 = new Gateway();
-    }
-
-    expect(createGW).not.toThrow();
+    var gw1 = new Gateway();
     expect(gw1.sock.readyState).toBe(1);
   });
 
-    it("constructor should cache Gateways to the same url+port", function() {
-    var gw1
-    var createGW = function(){
-       gw1 = new Gateway();
+  it("constructor should cache Gateways to the same url+port", function() {
+    var gw1 = new Gateway();
+    var gw2 = new Gateway();
+    expect(gw1).toBe(gw2);
+  });
+
+  it("registers itself with the global window.fjage object", function() {
+    var gw1 = new Gateway();
+    expect(window.fjage.gateways).toContain(gw1)
+  });
+
+});
+
+
+describe("An AgentID", function() {
+  var foo = 1;
+  it("should be able to be consuctured", function() {
+    var aid;
+    var createAID = function(){
+      aid = new AgentID('agent-name', true, gw);
     }
 
-    expect(createGW).not.toThrow();
-    expect(gw1).toBe(gw);
+    expect(createAID).not.toThrow();
+    expect(aid).toBeDefined();
   });
+
+  it("should have working getters and toString", function() {
+    var aid = new AgentID('agent-name', true, gw);
+    expect(aid.getName()).toBe('agent-name');
+    expect(aid.isTopic()).toBe(true);
+    expect(aid.toString()).toBe('#agent-name');
+  });
+
 });
+
+describe("An Message", function() {
+  var foo = 1;
+  it("should be able to be consuctured", function() {
+    var msg;
+    var createMSG = function(){
+      msg = new Message();
+    }
+
+    expect(createMSG).not.toThrow();
+    expect(msg).toBeDefined();
+  });
+
+  it("should have a unique msgID", function() {
+    var msg1 = new Message();
+    var msg2 = new Message();
+
+    expect(msg1.msgID).not.toBe(msg2.msgID);
+  });
+
+  it("should serialise and deserialise correctly.", function() {
+    var msg1 = new Message();
+    var msg2 = Message._deserialize(msg1._serialize());
+    var TxFrameReq = MessageClass('org.arl.unet.phy.TxFrameReq');
+    var txMsg = new TxFrameReq();
+
+
+    expect(msg1).toEqual(msg2);
+    expect(Message._deserialize(txMsg._serialize())).toEqual(txMsg);
+
+  });
+
+});
+
+
+
+
+
 
 
 function sendTestStatus(status){
