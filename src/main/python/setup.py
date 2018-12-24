eb82b17ffdf23fd3625449c3da096b93a9eaab39
Prasad Anjangi <prasadtiru@gmail.com>
2018-12-24 17:57:07 +0800

    refactor(setup): changes made in master moved to dev for fixing the pypi publishing

--- src/main/python/setup.py
+++ src/main/python/setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, find_packages
 
+with open('../../sphinx/pythongw.rst') as f:
+    with open('README.rst', 'w') as f1:
+        for line in f:
+            if ".. highlight" not in line:
+                f1.write(line)
+
 with open('README.rst') as f:
     readme = f.read()
 
@@ -9,8 +15,8 @@ with open('README.rst') as f:
 
 setup(
     name='fjagepy',
-    version='1.4.2b5',
-    description='Fjage Python Gateway',
+    version='1.5.2',
+    description='Python Gateway',
     long_description=readme,
     author='Prasad Anjangi, Mandar Chitre, Chinmay Pendharkar, Manu Ignatius',
     author_email='prasad@subnero.com, mandar@arl.nus.edu.sg, chinmay@subnero.com, manu@subnero.com',
@@ -18,9 +24,12 @@ setup(
     license='BSD (3-clause)',
     python_requires='>=3',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
     packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+        'numpy>=1.11'
+    ],
 )
