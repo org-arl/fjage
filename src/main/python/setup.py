284be7ae2c94c54af5dfe936bd721fe21719f3db
Prasad Anjangi <prasadtiru@gmail.com>
2018-12-24 16:14:18 +0800

    fix(setup): version 1.5.0 already existed, updated to 1.5.1

--- src/main/python/setup.py
+++ src/main/python/setup.py
@@ -15,7 +15,7 @@ with open('README.rst') as f:
 
 setup(
     name='fjagepy',
-    version='1.5.0',
+    version='1.5.1',
     description='Python Gateway',
     long_description=readme,
     author='Prasad Anjangi, Mandar Chitre, Chinmay Pendharkar, Manu Ignatius',
