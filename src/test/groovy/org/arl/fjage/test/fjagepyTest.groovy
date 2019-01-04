4df0ceb90b8d6165c465c0d8ee14200a7d08b5fc
Chinmay Pendharkar <chinmay@subnero.com>
2019-01-04 16:21:52 +0800

    fix(fjagepy): cleanup fjagepy tests which were giving false  native errors, also moved reconnection prints to logger

--- src/test/groovy/org/arl/fjage/test/fjagepyTest.groovy
+++ src/test/groovy/org/arl/fjage/test/fjagepyTest.groovy
@@ -1,12 +1,10 @@
 package org.arl.fjage.test
 
 import org.arl.fjage.*
-import org.arl.fjage.remote.*
 import org.arl.fjage.remote.MasterContainer
 import org.junit.Test
-import static org.junit.Assert.assertTrue
-import static org.junit.Assert.assertFalse
 
+import static org.junit.Assert.assertTrue
 
 class fjagepyTest {
 
@@ -29,15 +27,23 @@ class fjagepyTest {
 	      }
 	    })
 	    def ret = 0;
-	    println "Running automated tests.";
-	    def proc = "python src/test/groovy/org/arl/fjage/test/BasicTests.py".execute();
-	    def sout = new StringBuilder(), serr = new StringBuilder()
-	    proc.consumeProcessOutput(sout, serr)
-	    proc.waitFor();
-	    ret = proc.exitValue();
-	    println "NPM : out> $sout err> $serr \n ret = $ret \n testStatus = $testStatus"
-	    platform.shutdown()
-    	assertTrue(ret == 0 && testStatus)
+		if (System.getProperty('manualPyTest') == null){
+			println "Running automated tests.";
+			def proc = "python src/test/groovy/org/arl/fjage/test/BasicTests.py".execute();
+			def sout = new StringBuilder(), serr = new StringBuilder()
+			proc.consumeProcessOutput(sout, serr)
+			proc.waitFor();
+			ret = proc.exitValue();
+			println "Python : out> $sout err> $serr \n ret = $ret \n testStatus = $testStatus"
+		}else{
+			println "waiting for user to run manual tests"
+			while (!testStatus){
+				platform.delay(1000)
+			}
+		}
+		container.shutdown()
+		platform.shutdown()
+		assertTrue(ret == 0 && testStatus)
   	}
 
 }
