cbfda926bbc2c0a6e11b8401f0e8d258de5c0784
Prasad Anjangi <prasadtiru@gmail.com>
2020-10-09 15:26:03 +0800

    chore(gateway): updated fjagepy version number for release

--- gateways/python/setup.py
+++ gateways/python/setup.py
@@ -15,7 +15,7 @@ with open('README.rst') as f:
 
 setup(
     name='fjagepy',
-    version='1.7.0',
+    version='1.7.1',
     description='Python Gateway',
     long_description=readme,
     author='Prasad Anjangi, Mandar Chitre, Chinmay Pendharkar, Manu Ignatius',
