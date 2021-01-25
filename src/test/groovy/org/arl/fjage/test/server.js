d29952619f936eef2879627ce6c4501120818f6c
Chinmay Pendharkar <notthetup@gmail.com>
2021-01-25 16:31:08 +0800

    fix(fjagejs): improved fjagejs test telemetry into gradle and added a new test to verify async messages sending and receiving

--- src/test/groovy/org/arl/fjage/test/server.js
+++ src/test/groovy/org/arl/fjage/test/server.js
@@ -3,9 +3,12 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+  page.on('console', msg => {
+    msg.type() == "error" && console.log('PAGE ERR:', msg.text());
+    msg.type() == "warning" && console.log('PAGE WARN:', msg.text());
+  });
   await page.goto('http://127.0.0.1:8080/test', {waitUntil: 'networkidle2'});
-  await page.waitFor('.jasmine-overall-result');
-  await page.waitFor(100);
+  await page.waitForSelector('.jasmine-overall-result');
+  await page.waitForTimeout(100);
   await browser.close();
 })();
