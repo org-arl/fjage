org-arl/fjage

commit 9b4238a0436f469783550ae241b62c2b5c695bb6

feat(auth): throw AuthFailureException on failed request() from Gateway or Agent in SlaveContainer

src/main/java/org/arl/fjage/Agent.java
@@ -21,6 +21,7 @@ import java.util.function.Consumer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.arl.fjage.persistence.Store;
+import org.arl.fjage.remote.SlaveContainer;
 
 /**
  * Base class to be extended by all agents. An agent must be added to a container
@@ -504,7 +505,9 @@ public class Agent implements Runnable, TimestampProvider, Messenger {
 
   @Override
   public Message receive(final Message m, long timeout) {
-    return receive(new MessageFilter() {
+    if (container instanceof SlaveContainer)
+      ((SlaveContainer)container).checkAuthFailure(m.getMessageID());
+    Message rsp = receive(new MessageFilter() {
       private String mid = m.getMessageID();
       @Override
       public boolean matches(Message m) {
@@ -513,6 +516,10 @@ public class Agent implements Runnable, TimestampProvider, Messenger {
         return s.equals(mid);
       }
     }, timeout);
+    if (rsp != null) return null;
+    if (container instanceof SlaveContainer)
+      ((SlaveContainer)container).checkAuthFailure(m.getMessageID());
+    return null;
   }
 
   @Override

src/main/java/org/arl/fjage/remote/ConnectionHandler.java
@@ -27,10 +27,12 @@ class ConnectionHandler extends Thread {
   private final String ALIVE = "{\"alive\": true}";
   private final String SIGN_OFF = "{\"alive\": false}";
   private final int TIMEOUT = 5000;
+  private final int FAILED_SIZE = 256;
 
   private Connector conn;
   private DataOutputStream out;
   private Map<String,Object> pending = Collections.synchronizedMap(new HashMap<String,Object>());
+  private Deque<String> failed = new ArrayDeque<String>(FAILED_SIZE);
   private Logger log = Logger.getLogger(getClass().getName());
   private RemoteContainer container;
   private boolean alive, keepAlive, closeOnDead;
@@ -112,13 +114,19 @@ class ConnectionHandler extends Thread {
       try {
         JsonMessage rq = JsonMessage.fromJson(s);
         if (rq.action == null) {
-          if (fw.permit(rq) && rq.id != null) {
+          if (rq.id != null) {
             // response to some request
             Object obj = pending.get(rq.id);
             if (obj != null) {
               pending.put(rq.id, rq);
               synchronized(obj) {
                 obj.notify();
+              }
+            } else if (rq.auth != null) {
+              synchronized(failed) {
+                while (failed.size() >= FAILED_SIZE)
+                  failed.poll();
+                failed.offer(rq.id);
               }
             }
           }
@@ -245,6 +253,12 @@ class ConnectionHandler extends Thread {
     synchronized(watchList) {
       if (watchList.isEmpty()) return true;
       return watchList.contains(aid);
+    }
+  }
+
+  boolean checkAuthFailure(String id) {
+    synchronized(failed) {
+      return failed.contains(id);
     }
   }
 

src/main/java/org/arl/fjage/remote/Gateway.java
@@ -245,7 +245,9 @@ public class Gateway implements Messenger, Closeable {
 
   @Override
   public Message receive(final Message m, long timeout) {
-    return receive(new MessageFilter() {
+    if (container instanceof SlaveContainer)
+      ((SlaveContainer)container).checkAuthFailure(m.getMessageID());
+    Message rsp = receive(new MessageFilter() {
       private String mid = m.getMessageID();
       @Override
       public boolean matches(Message m) {
@@ -254,6 +256,10 @@ public class Gateway implements Messenger, Closeable {
         return s.equals(mid);
       }
     }, timeout);
+    if (rsp != null) return rsp;
+    if (container instanceof SlaveContainer)
+      ((SlaveContainer)container).checkAuthFailure(m.getMessageID());
+    return null;
   }
 
   @Override

src/main/java/org/arl/fjage/remote/SlaveContainer.java
@@ -33,7 +33,6 @@ public class SlaveContainer extends RemoteContainer {
   private ConnectionHandler master;
   private String hostname, settings;
   private int port, baud;
-  private Map<String,Object> pending = Collections.synchronizedMap(new HashMap<String,Object>());
   private boolean quit = false;
   private String watchListCache = null;
 
@@ -126,6 +125,15 @@ public class SlaveContainer extends RemoteContainer {
     return rsp != null && rsp.auth != null && rsp.auth;
   }
 
+  /**
+   * Checks for authentication failure on send.
+   * <p>
+   * INTERNAL USE ONLY.
+   */
+  public void checkAuthFailure(String id) {
+    if (master.checkAuthFailure(id)) throw new AuthFailureException();
+  }
+
   /////////////// Container interface methods to override
 
   @Override
@@ -138,7 +146,7 @@ public class SlaveContainer extends RemoteContainer {
     rq.id = UUID.randomUUID().toString();
     String json = rq.toJson();
     JsonMessage rsp = master.printlnAndGetResponse(json, rq.id, TIMEOUT);
-    return rsp != null && rsp.answer;
+    return rsp != null && rsp.answer != null && rsp.answer;
   }
 
   @Override
@@ -156,6 +164,7 @@ public class SlaveContainer extends RemoteContainer {
       if (!relay) return super.send(m, false);
       JsonMessage rq = new JsonMessage();
       rq.action = Action.SEND;
+      rq.id = m.getMessageID();
       rq.message = m;
       rq.relay = true;
       String json = rq.toJson();
@@ -166,6 +175,7 @@ public class SlaveContainer extends RemoteContainer {
       if (!relay) return false;
       JsonMessage rq = new JsonMessage();
       rq.action = Action.SEND;
+      rq.id = m.getMessageID();
       rq.message = m;
       rq.relay = true;
       String json = rq.toJson();

src/test/java/org/arl/fjage/test/BasicTests.java
@@ -216,8 +216,12 @@ public class BasicTests {
       // all good
     }
     Message req = new RequestMessage(server.getAgentID());
-    Message rsp = gw.request(req, 1000);
-    assertNull(rsp);
+    try {
+      gw.request(req, 1000);
+      fail("Should have thrown AuthFailureException");
+    } catch (AuthFailureException ex) {
+      // all good
+    }
     gw.close();
     platform.shutdown();
     assertEquals(0, server.requests);

commit a6562b6093c1bb77ee33157260f0aa90e7a5baff

fix(core): fix bug introduced in 9b4238a0 in Agent

src/main/java/org/arl/fjage/Agent.java
@@ -516,7 +516,7 @@ public class Agent implements Runnable, TimestampProvider, Messenger {
         return s.equals(mid);
       }
     }, timeout);
-    if (rsp != null) return null;
+    if (rsp != null) return rsp;
     if (container instanceof SlaveContainer)
       ((SlaveContainer)container).checkAuthFailure(m.getMessageID());
     return null;