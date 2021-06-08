204e4656a22cad0aa007f642adc75d1978d6d47f
Chinmay Pendharkar <notthetup@gmail.com>
2021-06-08 21:24:37 +0800

    fix(fjagejs): cleanup depdendencies

--- gateways/js/rollup.config.js
+++ gateways/js/rollup.config.js
@@ -1,14 +1,14 @@
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import { terser } from "rollup-plugin-terser";
-import babel from "@rollup/plugin-babel";
-import pkg from "./package.json"
-import { spawn } from 'child_process'
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import babel from '@rollup/plugin-babel';
+import pkg from './package.json';
+import { spawn } from 'child_process';
 
 const ls = spawn('git', ['describe', '--always', '--abbrev=8', '--match', 'NOT A TAG', '--dirty=*']);
 var commit;
-ls.stdout.on('data', (data) => { commit = data });
+ls.stdout.on('data', (data) => { commit = data; });
 
-const input = ["src/fjage.js"];
+const input = ['src/fjage.js'];
 export default [
   {
     // UMD
@@ -16,37 +16,37 @@ export default [
     plugins: [
       nodeResolve(),
       babel({
-        babelHelpers: "bundled",
+        babelHelpers: 'bundled',
       }),
       terser(),
     ],
     output: {
       file: `dist/${pkg.name}.min.js`,
-      format: "umd",
-      name: "fjage",
+      format: 'umd',
+      name: 'fjage',
       esModule: false,
-      exports: "named",
+      exports: 'named',
       sourcemap: true,
     },
   },
-// ESM and CJS
+  // ESM and CJS
   {
     input,
     plugins: [nodeResolve()],
     output: [
       {
-        format: "esm",
-        exports: "named",
-        dir : `dist/mjs`,
-        chunkFileNames: "[name].js",
+        format: 'esm',
+        exports: 'named',
+        dir : 'dist/mjs',
+        chunkFileNames: '[name].js',
         banner : `/* fjage.js v${pkg.version}${commit?'/'+commit:''} ${new Date().toISOString()} */\n`
       },
       {
-        format: "cjs",
-        exports: "named",
-        dir : `dist/cjs`,
-        chunkFileNames: "[name].cjs",
-        entryFileNames: "[name].cjs",
+        format: 'cjs',
+        exports: 'named',
+        dir : 'dist/cjs',
+        chunkFileNames: '[name].cjs',
+        entryFileNames: '[name].cjs',
         banner : `/* fjage.js v${pkg.version}${commit?'/'+commit:''} ${new Date().toISOString()} */\n`
       },
     ],
