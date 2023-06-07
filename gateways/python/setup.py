6df2aab71c647c1e35df5d098a0ca84179fd313a
Chinmay Pendharkar <notthetup@gmail.com>
2023-06-07 14:36:52 +0800

    chore(fjagepy): bumping version

--- gateways/python/setup.py
+++ gateways/python/setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='fjagepy',
-    version='1.7.5',
+    version='1.7.6.dev1',
     description='Python Gateway',
     long_description=readme,
     author='Prasad Anjangi, Mandar Chitre, Chinmay Pendharkar, Manu Ignatius',
