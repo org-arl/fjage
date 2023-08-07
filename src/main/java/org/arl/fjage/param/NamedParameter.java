786d6a6da9aaad1eeb39b9fa5456d396cd5a2875
Mandar Chitre <mandar@nus.edu.sg>
2023-08-08 00:41:17 +0800

    feat(param): allow named params to match with param enums in get() (#284)

--- src/main/java/org/arl/fjage/param/NamedParameter.java
+++ src/main/java/org/arl/fjage/param/NamedParameter.java
@@ -74,20 +74,22 @@ public class NamedParameter implements Parameter, Serializable {
   }
 
   /**
-   * Compares if two named parameters are equals.
+   * Compares if a parameter is equivalent to the named parameter.
    *
    * @return true if two parameters are equal
    */
   @Override
   public boolean equals(Object obj) {
-    if (!(obj instanceof NamedParameter)) return false;
-    NamedParameter p = (NamedParameter)obj;
+    if (!(obj instanceof Parameter)) return false;
     String name1 = name;
     if (name1.contains(".")) name1 = name1.substring(name1.indexOf('.')+1);
-    String name2 = p.name;
+    String name2 = obj.toString();
     if (name2.contains(".")) name2 = name2.substring(name2.indexOf('.')+1);
     if (!name1.equals(name2)) return false;
-    if (ord >= 0 && p.ord >= 0 && p.ord != ord) return false;
+    if (obj instanceof NamedParameter) {
+      NamedParameter p = (NamedParameter)obj;
+      if (ord >= 0 && p.ord >= 0 && p.ord != ord) return false;
+    }
     return true;
   }
 
