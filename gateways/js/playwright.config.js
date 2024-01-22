0be3713590dd8178328e1a7098147b377af96ee1
Chinmay Pendharkar <notthetup@gmail.com>
2024-01-22 10:34:40 +0800

    fix(fjagejs): adding playwright install before testing

---
+++ gateways/js/playwright.config.js
@@ -0,0 +1,19 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  projects: [
+    /* Test against desktop browsers */
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    }
+  ],
+});
