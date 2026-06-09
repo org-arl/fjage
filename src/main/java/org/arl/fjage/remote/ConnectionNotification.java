a9d18fe07d3cd4c3469c061ed98538c5622169c4
Chinmay Pendharkar <chinmay@subnero.com>
2026-06-09 17:59:05 +0800

    feat(tunnel): adding connID and connection notifications for tunnel

---
+++ src/main/java/org/arl/fjage/remote/ConnectionNotification.java
@@ -0,0 +1,69 @@
+package org.arl.fjage.remote;
+
+import org.arl.fjage.*;
+
+/**
+ * Notification message sent by `Tunnel` when a connector is opened or closed.
+ * <p>
+ * This message carries a short `event` string (e.g., "connected", "closed"),
+ * the numeric `connID` assigned by the tunnel, the connector `name`.
+ */
+public class ConnectionNotification extends Message {
+
+  private static final long serialVersionUID = 1L;
+
+  private String event;
+  private int connID;
+  private String name;
+
+  public ConnectionNotification() {
+    super();
+    setPerformative(Performative.INFORM);
+  }
+
+  public ConnectionNotification(String event, int connID, String name) {
+    super();
+    setPerformative(Performative.INFORM);
+    this.event = event;
+    this.connID = connID;
+    this.name = name;
+  }
+
+  public ConnectionNotification(AgentID recipient, String event, int connID, String name) {
+    super(recipient);
+    setPerformative(Performative.INFORM);
+    this.event = event;
+    this.connID = connID;
+    this.name = name;
+  }
+
+  public String getEvent() {
+    return event;
+  }
+
+  public void setEvent(String event) {
+    this.event = event;
+  }
+
+  public int getConnID() {
+    return connID;
+  }
+
+  public void setConnID(int connID) {
+    this.connID = connID;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return "ConnectionNotification[event="+event+", connID="+connID+", name="+name+"]";
+  }
+
+}
