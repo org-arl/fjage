0abbf18d6caca2fd5d83707ba12549091d0674a6
Chinmay Pendharkar <chinmay@subnero.com>
2026-09-09 10:45:08 +0800

    test(run-tests): remove minified-spec from Node.js test execution

--- gateways/js/test/run-tests.cjs
+++ gateways/js/test/run-tests.cjs
@@ -45,7 +45,7 @@ if (serveBrowserTest) {
   if (runNodeTest) {
     console.log('Running jasmine..');
     let startTime = new Date();
-    await runJasmine('test/spec/', ['fjageSpec.cjs', 'minified-spec.cjs']);
+    await runJasmine('test/spec/', ['fjageSpec.cjs']);
     console.log('Node.js test Complete [' + (new Date() - startTime) + ' ms]');
   }
 
